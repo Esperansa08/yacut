4cbe88c71921d1c24738902299d7411ce3e778d5
Анастасия Савельева <Visteria09@yandex.ru>
2023-08-21 17:51:20 +0700

    добавлены дефолтные значения

--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ SYMBOLS_URL = string.ascii_letters + string.digits
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='fhgdtrew45dgik')
